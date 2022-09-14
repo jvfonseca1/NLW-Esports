747d00be2954c06d0d96d3392a4b1542c83709ff
João Victor Fonseca <joao.fonseca.victor@gmail.com>
2022-09-14 09:10:47 -0300

    Format Changes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -26,8 +26,8 @@ function App() {
           <img src="/Game-2.png" alt="" />
 
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>Dota 2</strong>
-          <span className='text-zinc-300 text-sm block '>2 anúncuios</span>
+            <strong className='font-bold text-white block'>Dota 2</strong>
+            <span className='text-zinc-300 text-sm block '>2 anúncuios</span>
           </div>
         </a>
 
@@ -35,8 +35,8 @@ function App() {
           <img src="/Game-3.png" alt="" />
 
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>Counter Strike</strong>
-            <span className='text-zinc-300 text-sm block '>5 anúncuios</span>
+              <strong className='font-bold text-white block'>Counter Strike</strong>
+              <span className='text-zinc-300 text-sm block '>5 anúncuios</span>
           </div>
         </a>
 
