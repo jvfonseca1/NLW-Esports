826f35ee9c49e973539b32ac56f065c03d3ccba0
João Victor Fonseca <joao.fonseca.victor@gmail.com>
2022-09-17 17:58:48 -0300

    Revert "Separated Functions in Components"
    This reverts commit 0165b1fed57496de2e96e75290e3941f7626e2d1.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
 import './styles/main.css';
 
 import logoimg from './assets/logo-nlw.svg';
+<<<<<<< HEAD
 import { GameBanner } from './components/GameBanner/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner/CreateAdBanner';
 import { useEffect, useState } from 'react';
+=======
+import {MagnifyingGlassPlus} from "phosphor-react";
+>>>>>>> parent of 0165b1f (Separated Functions in Components)
 
 // Tipagem Game
 interface Game {
@@ -37,6 +41,7 @@ function App() {
       </h1>
 
       {/* Grid de Jogos */}
+<<<<<<< HEAD
       <div className='grid grid-cols-6 gap-6 mt-16'>
         
         {games.map(game => {
@@ -54,6 +59,79 @@ function App() {
       
       {/* Postagem de Anúncios */}
       <CreateAdBanner/>
+=======
+      <div id='games-grid' className='grid grid-cols-6 gap-6 mt-16'>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/Game-1.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 text-sm block '>4 anúncuios</span>
+          </div>
+        </a>
+      
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/Game-2.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Dota 2</strong>
+            <span className='text-zinc-300 text-sm block '>2 anúncuios</span>
+          </div>
+        </a>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/Game-3.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>Counter Strike: Global Offensive</strong>
+              <span className='text-zinc-300 text-sm block '>5 anúncuios</span>
+          </div>
+        </a>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/Game-4.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Apex Legends</strong>
+            <span className='text-zinc-300 text-sm block '>7 anúncuios</span>
+          </div>
+        </a>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/Game-5.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Fortnite</strong>
+            <span className='text-zinc-300 text-sm block '>15 anúncuios</span>
+          </div>
+        </a>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/Game-6.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>World of Warcraft</strong>
+            <span className='text-zinc-300 text-sm block '>3 anúncuios</span>
+          </div>
+        </a>
+
+      </div>
+      
+      {/* Postagens de Anúncios */}
+      <div className='pt-1 bg-nlw-gradient mt-8 self-stretch rounded-lg overflow-hidden '>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center '>
+          <div>
+            <h1 className='font-black text-2xl text-white'>Não encontrou seu duo?</h1>
+            <span className='text-zinc-400 text-base font-normal'>Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className='px-4 py-3 bg-violet-500 rounded text-white hover:bg-violet-600 flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
+>>>>>>> parent of 0165b1f (Separated Functions in Components)
 
     </div>
   )
